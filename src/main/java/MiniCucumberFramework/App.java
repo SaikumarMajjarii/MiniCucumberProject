824d24501892ef459cc8d5df49dd8c84c65eb2cb
Saikumar Majjari <saikumar.majjarii@gmail.com>
2023-04-22 12:47:45 +0530

    Title: Mini Cucumber Project

---
+++ src/main/java/MiniCucumberFramework/App.java
@@ -0,0 +1,13 @@
+package MiniCucumberFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
